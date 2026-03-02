efb5985471143b55f316b58c0e8eac666e188b06
Shahabaj Mulani <mulanishahabaj90@gmail.com>
2026-03-02 12:32:22 +0530

    commit

--- barkat/shop/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import Cart
-
-def cart_count(request):
-    count = 0
-    if request.session.get('user'):
-        count =Cart.objects.filter(username=request.session['user']).count()
-    return {'cart_count': count}    
-
-#! settings.py line 65
